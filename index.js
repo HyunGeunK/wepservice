e6e40e7cb0be4b46f9dec6455bdb8bd4f29a978a
HyunGeunK <hyungeun0313@naver.com>
2024-04-30 09:19:10 +0900

    dalle3 api연동

---
+++ index.js
@@ -0,0 +1,18 @@
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+    apiKey: "",
+});
+
+async function createImage(){
+    const response = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: "",
+        n: 1,
+        size: "1024x1024",
+    });
+    image_url = response.data[0].url;
+    console.log(image_url); 
+}
+createImage();
+
